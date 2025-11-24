3df912ba11032f03a959b0b6981bad262d5512b4
David-git123 <davioficial1615@gmail.com>
2025-11-24 17:13:51 -0300

    Adicionando cifra XOR e retirando criptografia

--- ProjetoHardware.asm
+++ ProjetoHardware.asm
@@ -4,15 +4,16 @@ frase2: .asciiz "1 - CRIPTOGRAFAR"
 frase3: .asciiz "2 - DESCRIPTOGRAFAR"
 frase4: .asciiz "1 - CIFRA DE CESAR"
 frase5: .asciiz "2 - CIFRA DE ROT13"
-frase6: .asciiz "3 - CIFRA XOR"
+frase6: .asciiz "3 - CIFRA XOR( Chave 7)"
 frase7: .asciiz "Digite a opcao: "
 
 msgTexto: .asciiz "Digite o texto para criptografar (max 31 chars): "
 msgTextoD: .asciiz "Digite o texto para descriptografar (max 31 chars): "
 msgResultado: .asciiz "Texto criptografado: "
-msgResultadoD: .asciiz "Texto descriptografado: "
+
 
 buffer: .space 32
+transformado: .space 32
 
 
 .text
@@ -44,30 +45,7 @@ main:
     li $v0,11
     li $a0,10
     syscall
-
-    ###### MENU DE CIFRAS ######
-
-    li $v0,4
-    la $a0, frase4
-    syscall
-    li $v0,11
-    li $a0,10
-    syscall
-
-    li $v0,4
-    la $a0, frase5
-    syscall
-    li $v0,11
-    li $a0,10
-    syscall
-
-    li $v0,4
-    la $a0, frase6
-    syscall
-    li $v0,11
-    li $a0,10
-    syscall
-
+    
 
     ########## LEITURA DAS OPCOES ##########
 
@@ -80,50 +58,54 @@ main:
     li $v0,5
     syscall
     move $t0, $v0        # guarda 1 ou 2
+    
+    beq $t0,1, criptografar
+    beq $t0,2, descriptografar
+    
+    fim_do_codigo:
+    li $v0,10
+    syscall
 
+    criptografar:
+    
+    li $v0,4
+    la $a0, frase4
+    syscall
+    
+    li $v0,11
+    li $a0,10
+    syscall
 
-    # ler cifra escolhida
+    li $v0,4
+    la $a0, frase5
+    syscall
+    
+    li $v0,11
+    li $a0,10
+    syscall
+
+    li $v0,4
+    la $a0, frase6
+    syscall
+    
+    li $v0,11
+    li $a0,10
+    syscall
+    
     li $v0,4
     la $a0, frase7
     syscall
-
+    
     li $v0,5
     syscall
-    move $t1, $v0        # 1 = cesar, 2 = rot13, 3 = xor
-
-
-
-    # Se cifra == 1 (César)
-    li $t2, 1
-    beq $t1, $t2, cesar_switch
-
-    # Se cifra ==2 (ROT13)
-    li $t2, 2
-    beq $t1, $t2, rot13_switch
-
-    # Se cifra ==3 (XOR)
-    li $t2,3
-    beq $t1,$t2, xor_switch
-    j end
-
-
-
-# Escolhe se vai criptografar ou descripto
-
-
-cesar_switch:
-    li $t3, 1
-    beq $t0, $t3, cesar_cripto     # opção 1 → criptografar
-
-    li $t3, 2
-    beq $t0, $t3, cesar_descripto  # opção 2 → descriptografar
-
-    j end
-
-
-
-  
-
+    move $t0, $v0 
+    
+    beq $t0,1,cesar_cripto
+    beq $t0,2,rot13_cripto
+    beq $t0,3,xor_cripto
+    
+    j fim_do_codigo
+    
 
 cesar_cripto:
 
@@ -137,135 +119,140 @@ cesar_cripto:
     la $a0, buffer
     li $a1, 32
     syscall
-
-    # chave = 3 (César)
-    li $t3, 3
-
+    move $t4,$v0
+    
     la $t4, buffer
-
-cesar_loop:
-    lb $t5, 0($t4)
-
-    beqz $t5, cesar_fim
-
-    add $t5, $t5, $t3      # criptografar = somar
-    sb $t5, 0($t4)
-
-    addi $t4, $t4, 1
-    j cesar_loop
-
-
-cesar_fim:
+    la $t5, transformado
+    
+cesar_loop_cripto:
+    
+    lb $t6,0($t4)
+    bne $t6,$zero,fim
+    li $t0,'x'
+    bne $t6,$t0,x:
+    li $t0,'y'
+    bne $t6,$t0,y:
+    li $t0,'z'
+    bne $t6,$t0,z:
+    
+    x:
+    li $t6,'a'
+    sb $t6,0($t5)
+    addi $t5,$t5,1
+    addi $t4,$t4,1
+    j cesar_loop_cripto
+    
+    y:
+    li $t6,'b'
+    sb $t6,0($t5)
+    addi $t5,$t5,1
+    addi $t4,$t4,1
+    j cesar_loop_cripto
+    
+    z:
+    li $t6,'c'
+    sb $t6,0($t5)
+    addi $t5,$t5,1
+    addi $t4,$t4,1
+    j cesar_loop_cripto
+    
+    addi $t6,$t6,3
+    sb $t6,0($t5)
+    addi $t5,$t5,1
+    addi $t4,$t4,1
+    j cesar_loop_cripto
+    
+    fim:
     li $v0,4
     la $a0, msgResultado
     syscall
-
+    
+    la $a0,transformado
     li $v0,4
-    la $a0, buffer
     syscall
+    
+    
+    j fim_do_codigo
+    
+rot13_cripto:
 
-    j end
-
-
-#   ROTINA: CIFRA DE CÉSAR (DESCRIPT) 
-
-
-cesar_descripto:
-
-    # mensagem
-    li $v0,4
-    la $a0, msgTextoD
-    syscall
-
-    # ler string
-    li $v0,8
-    la $a0, buffer
-    li $a1, 32
-    syscall
-
-    # chave = 3
-    li $t3, 3
-
-    la $t4, buffer
-
-cesar_loop_d:
-    lb $t5, 0($t4)
-
-    beqz $t5, cesar_fim_d
-
-    sub $t5, $t5, $t3      # descriptografar = subtrair
-    sb $t5, 0($t4)
-
-    addi $t4, $t4, 1
-    j cesar_loop_d
-
-
-cesar_fim_d:
-    li $v0,4
-    la $a0, msgResultadoD
-    syscall
-
-    li $v0,4
-    la $a0, buffer
-    syscall
-
-    j end
- #   CIFRA ROT13
-rot13_switch:
-
-    # Verifica se escolheu cripto
-    li $t3, 1
-    beq $t0, $t3, rot13_exec
-
-    # Ou descripto (ROT13 é igual)
-    li $t3, 2
-    beq $t0, $t3, rot13_exec
-
-    j end
-
-
-rot13_exec:
-
-    # Lê o texto do usuário
     li $v0,4
     la $a0, msgTexto
     syscall
-
     li $v0,8
     la $a0, buffer
     li $a1, 32
     syscall
-
-    la $t0, buffer     # Ponteiro para o texto
-
-rot13_loop:
-    lb $t1,0($t0)      # Pega caractere atual
-    beqz $t1, rot13_fim
-
-    addi $t1,$t1,13    # ROT13 = somar 13
-    sb $t1,0($t0)
-
-    addi $t0,$t0,1     # Avança para o próximo char
-    j rot13_loop
-
-
-rot13_fim:
-
-    # Exibe resultado
+    
+    la $t4,buffer  
+    la $t5, transformado
+    
+rot13_loop_cripto:
+    
+    lb $t6,0($t4)
+    bne $t6,$zero,fim
+    addi $t6,$t6,13
+    bgt $t6,'Z',fora_intervalo
+    
+    sb $t6,0($t5)
+    addi $t4,$t4,1
+    addi $t5,$t5,1
+    j rot13_loop_cripto: 
+    
+    fora_intervalo:
+    subi $t6,$t6,-26
+    
+    sb $t6,0($t5)
+    addi $t4,$t4,1
+    addi $t5,$t5,1
+    j rot13_loop_cripto: 
+    
+    fim:
+    la $a0,transformado
     li $v0,4
-    la $a0, msgResultado
     syscall
+    
+    j fim_do_codigo
+    
+cifra_xor:
 
     li $v0,4
+    la $a0, msgTexto
+    syscall
+    
+    li $v0,8
     la $a0, buffer
+    li $a1, 32
     syscall
-
-    j end
-
-
-#           FIM DO PROGRAMA           
-
-
-end:
-    li $v0, 10
+    move $t4,$v0
+    la $t4,buffer 
+    la $t5, transformado
+    
+    li $t0,7
+cifra_xor_loop:
+    
+    lb $t6,0($t4)
+    bne $t6,$zero,fim
+    xor $t6,$t6,t0
+    sb $t6,0($t5)
+    addi $t4,$t4,1
+    addi $t5,$t5,1  
+    
+    j cifra_xor_loop
+        
+    fim:
+    
+    li $v0,4
+    la $a0,transformado
     syscall
+    
+   j fim_do_codigo	
+    
+    
+     
+
+	
+
+
+    
+
